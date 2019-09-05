954d120a33d21c6644cb5209dd35db715b73516a
limjoonchul <dlawnscjf511@naver.com>
2019-09-05 16:17:01 +0900

    이클립스 uml 스타크래프트

---
+++ myWorkspace/uml/src/main/java/kr/ac/daelim/uml/Mdeic.java
@@ -0,0 +1,24 @@
+package kr.ac.daelim.uml;
+
+public class Mdeic {
+	
+	public void move() {
+		System.out.println("유닛이 이동한다");
+		
+	}
+	public void healing(Chactor charactor) {
+		
+		System.out.println("유닛이 치료한다.");
+	}
+	public void underAttack(Chactor charactor) {
+		
+		System.out.println("유닛이 공격받는다.");
+	}
+
+
+	
+	
+
+
+
+}
---
+++ myWorkspace/uml/src/main/java/kr/ac/daelim/uml/Unit.java
@@ -0,0 +1,16 @@
+package kr.ac.daelim.uml;
+
+public class Unit {
+	public String name;
+	public int health;
+	public void move() {
+		System.out.println("유닛이 이동한다");
+	}
+	public void underAttack(Chactor charactor) {
+		
+		System.out.println("유닛이 공격받는다.");
+	}
+
+
+
+}
